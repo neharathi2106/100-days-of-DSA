68c25af774be0286f25dd4056b172748d88cc4b0
Neha Rathi <neharathi69@Nehas-MacBook-Air.local>
2026-03-02 21:15:57 +0530

    30th commit

--- day3.c
+++ day3.c
@@ -4,13 +4,13 @@ int main() {
     int n, k, comparisons = 0;
     int arr[100];
     int found = -1;
-    
+
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
     scanf("%d", &k);
-    
+
     for (int i = 0; i < n; i++) {
         comparisons++;
         if (arr[i] == k) {
@@ -18,13 +18,13 @@ int main() {
             break;
         }
     }
-    
+
     if (found != -1) {
         printf("Found at index %d\n", found);
     } else {
         printf("Not Found\n");
     }
     printf("Comparisons = %d\n", comparisons);
-    
+
     return 0;
 }
