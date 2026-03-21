d4b46e3668600e049f09dd2a5c301b035cefbe20
Neha Rathi <neharathi69@Nehas-MacBook-Air.local>
2026-03-21 14:33:33 +0530

    45th commit

---
+++ day45.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) {
+    if (data == -1)
+        return NULL;
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+int height(struct Node* root) {
+    if (root == NULL)
+        return 0;
+    int left = height(root->left);
+    int right = height(root->right);
+    return (left > right ? left : right) + 1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* nodes[n];
+
+    for (int i = 0; i < n; i++)
+        nodes[i] = createNode(arr[i]);
+
+    int j = 1;
+
+    for (int i = 0; i < n && j < n; i++) {
+        if (nodes[i] != NULL) {
+            if (j < n)
+                nodes[i]->left = nodes[j++];
+            if (j < n)
+                nodes[i]->right = nodes[j++];
+        }
+    }
+
+    printf("%d", height(nodes[0]));
+
+    return 0;
+}
