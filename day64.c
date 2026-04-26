6b223aef70cab4d524199f0a74ccf67ff72029d4
Neha Rathi <neharathi754@gmail.com>
2026-04-26 22:29:51 +0530

    64th commit

---
+++ day64.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void addEdge(struct Node* adj[], int u, int v) {
+    struct Node* newNode = createNode(v);
+    newNode->next = adj[u];
+    adj[u] = newNode;
+
+    newNode = createNode(u);
+    newNode->next = adj[v];
+    adj[v] = newNode;
+}
+
+void bfs(struct Node* adj[], int n, int s) {
+    int visited[n];
+    for (int i = 0; i < n; i++)
+        visited[i] = 0;
+
+    int queue[n];
+    int front = 0, rear = 0;
+
+    visited[s] = 1;
+    queue[rear++] = s;
+
+    while (front < rear) {
+        int curr = queue[front++];
+        printf("%d ", curr);
+
+        struct Node* temp = adj[curr];
+        while (temp) {
+            int neighbor = temp->data;
+            if (!visited[neighbor]) {
+                visited[neighbor] = 1;
+                queue[rear++] = neighbor;
+            }
+            temp = temp->next;
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d", &n);
+    scanf("%d", &m);
+
+    struct Node* adj[n];
+    for (int i = 0; i < n; i++)
+        adj[i] = NULL;
+
+    int u, v;
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        addEdge(adj, u, v);
+    }
+
+    int s;
+    scanf("%d", &s);
+
+    bfs(adj, n, s);
+
+    return 0;
+}
